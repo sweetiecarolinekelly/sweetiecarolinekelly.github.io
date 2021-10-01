2099f67bcdb9a0ce309b818698c2b3ac967b9af7
Caroline Kelly <89622318+sweetiecarolinekelly@users.noreply.github.com>
2021-09-30 23:07:07 -0500

    WEEK 6

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+var  myCanvas=createCanvas(windowWidth, windowHeight);
+myCanvas.parent('canvasDiv');
+  //background(147,112,219);
+}
+function draw() {
+textSize(30);
+textFont('Georgia');
+text('Kelly',mouseX,mouseY,40,40);
+fill(207,207,196);
+text('Caroline',mouseX, mouseY);
+fill(255,255,255);
+}
+  
